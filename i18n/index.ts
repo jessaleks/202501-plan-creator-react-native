b1921c88fdcdbed5bf9c1eb1b5c9cb8190457b80
Aleksander Jess <ja@aleksanderjess.com>
2025-01-13 01:49:20 +0100

    added i18n

---
+++ i18n/index.ts
@@ -0,0 +1,33 @@
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import * as Localization from "expo-localization";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import translationPL from '@/i18n/locales/pl/translationPL.json';
+import translationEN from '@/i18n/locales/en/translationEN.json'
+
+const resources = {
+    "pl-PL": {translation: translationPL},
+    "en-US": {translation: translationEN},
+};
+
+const initI18n = async () => {
+    let savedLanguage = await AsyncStorage.getItem("language");
+
+    if (!savedLanguage) {
+        savedLanguage = Localization.getLocales()[0].languageTag;
+    }
+
+    i18n.use(initReactI18next).init({
+        compatibilityJSON: "v4",
+        resources,
+        lng: savedLanguage,
+        fallbackLng: "en-US",
+        interpolation: {
+            escapeValue: false,
+        },
+    });
+};
+
+initI18n();
+
+export default i18n;
