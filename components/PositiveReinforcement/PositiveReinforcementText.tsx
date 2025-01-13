a8178f15fff6feb37214505578c0fa2b6e54841b
Aleksander Jess <ja@aleksanderjess.com>
2025-01-13 11:09:33 +0100

    renaming

--- components/PositiveReinforcement/PositiveReinforcementText.tsx
+++ components/PositiveReinforcement/PositiveReinforcementText.tsx
@@ -3,7 +3,7 @@ import {ThemedText} from "@/components/ThemedText";
 
 type PositiveReinforcementProps = {}
 
-export default function PositiveReinforcement({}: PositiveReinforcementProps) {
+export default function PositiveReinforcementText({}: PositiveReinforcementProps) {
     return (
         <ThemedView>
             <ThemedText>Hello from PositiveReinforcement.tsx</ThemedText>
