c6707c51aa17fdf879af736c4f5aa72279e9abcb
Aleksander Jess <ja@aleksanderjess.com>
2025-01-19 20:21:28 +0100

    Add TODO for i18n text integration in BreakText component

--- components/BreakText/BreakText.tsx
+++ components/BreakText/BreakText.tsx
@@ -4,6 +4,7 @@ import { ThemedText } from "@/components/ThemedText/ThemedText";
 type BreakTextProps = {};
 
 export default function BreakText({}: BreakTextProps) {
+	// TODO gets texts from i18n/locales/en/translationEN.json or i18n/locales/pl/translationPL.json and outputs random one
 	return (
 		<ThemedView>
 			<ThemedText>Hello from BreakText.tsx</ThemedText>
