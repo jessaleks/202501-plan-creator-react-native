ad302310bc8866aa7881f7e7537763abc314ab87
Aleksander Jess <ja@aleksanderjess.com>
2025-02-06 14:23:08 +0100

    Add initial macOS and Windows project files for PlanCreator

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,45 @@
-const {getDefaultConfig} = require('expo/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const fs = require("node:fs");
+const path = require("node:path");
+const exclusionList = require("metro-config/src/defaults/exclusionList");
 
-config.resolver.sourceExts.push('sql'); // <--- add this
+const rnwPath = fs.realpathSync(
+	path.resolve(require.resolve("react-native-windows/package.json"), ".."),
+);
 
-module.exports = config;
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+const config = {
+	//
+	resolver: {
+		blockList: exclusionList([
+			// This stops "npx @react-native-community/cli run-windows" from causing the metro server to crash if its already running
+			new RegExp(
+				`${path.resolve(__dirname, "windows").replace(/[/\\]/g, "/")}.*`,
+			),
+			// This prevents "npx @react-native-community/cli run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
+			new RegExp(`${rnwPath}/build/.*`),
+			new RegExp(`${rnwPath}/target/.*`),
+			/.*\.ProjectImports\.zip/,
+		]),
+		//
+	},
+	transformer: {
+		getTransformOptions: async () => ({
+			transform: {
+				experimentalImportSupport: false,
+				inlineRequires: true,
+			},
+		}),
+	},
+};
+
+config.resolver?.sourceExts?.concat([...config.resolver.sourceExts, "sql"]);
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
---
+++ windows/PlanCreator/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::PlanCreator::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::PlanCreator::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
